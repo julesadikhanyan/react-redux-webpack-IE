0cb319fe5d7649a0b2282b52634b959d3581153e
jules <jules.adikhanyan@gmail.com>
2021-11-08 16:06:02 +0300

    Basic template

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return (
+        <h1>USERS POSTS</h1>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+    entry: {
+        app: ["core-js/stable", "./src/index.js"]
+    },
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "index_bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./public/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
