c765f9f41f3380c6671b94c939e4ba03e6583253
jules <jules.adikhanyan@gmail.com>
2021-11-17 11:54:13 +0300

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
     mode: "production",
     entry: {
@@ -24,16 +24,27 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.svg$/,
+                loader: "svg-inline-loader"
             }
         ]
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: "./public/index.html"
+            template: "./public/index.html",
+            favicon: "./public/favicon.svg"
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './public/favicon.svg' },
+            ]
+        })
     ],
     devServer: {
         historyApiFallback: true
-    }
+    },
+
 }
